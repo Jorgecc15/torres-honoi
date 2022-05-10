0f43ea44369f366b5813f86baecd8fccc1dfc60e
Jorgecc15 <92415699+Jorgecc15@users.noreply.github.com>
2022-05-09 20:38:19 -0500

    Update hanoi.js

--- hanoi.js
+++ hanoi.js
@@ -1,54 +1,55 @@
 
 let respuesta=""
-
-function hanoi(discos,origen,final) {
-    if (discos==1) {
-        respuesta=respuesta + origen + " -> " + final + "&nbsp;"+ "&nbsp;"
-    } else{
-        if (final==3&&origen==1) {
-            nuevo_final=2
-        }
-        if (final==3&&origen==2) {
-            nuevo_final=1
-        }
-        if (final==2&&origen==1) {
-            nuevo_final=3
-        }
-        if (final==2&&origen==3) {
-            nuevo_final=1
-        }
-        if (final==1&&origen==3) {
-            nuevo_final=2
-        }
-        if (final==1&&origen==2) {
-            nuevo_final=3
-        }
-        hanoi(discos-1,origen,nuevo_final)
-        respuesta= respuesta + origen + " -> " + final + "&nbsp;"+ "&nbsp;"
-        nuevo_origen = nuevo_final
-        nuevo_final=final
-        hanoi(discos-1, nuevo_origen, nuevo_final)
-    }
-}
-
+let n
+let origen
+let destino
 do {
-    discos=Number(prompt("¿Cuantos discos?"))    
-} while (discos>10|| discos<1||isNaN(discos));
+    n=Number(prompt("¿Cuantos discos?"))    
+} while (n>10|| n<1||isNaN(n));
 do {
     origen=Number(prompt("¿Cual es el orgien de los discos? (1, 2 o 3)"))    
 } while (origen!=1 && origen!=2 && origen!=3 || isNaN(origen));
 do {
-    final=Number(prompt("¿Cual es el destino de los discos? (1, 2 o 3)"))    
-} while (final!=1 && final!=2 && final!=3 || isNaN(final));
+    destino=Number(prompt("¿Cual es el destino de los discos? (1, 2 o 3)"))    
+} while (destino!=1 && destino!=2 && destino!=3 || isNaN(destino));
 
-if(final==origen)
+function hanoi(n,origen, destino){
+    if(n==1){
+        respuesta=respuesta + origen + " -> " + destino + "&nbsp;"+ "&nbsp;"
+    }else{
+    if(origen==1 && destino==2){
+        nuevo_final=3
+    }
+    if (origen==1&&destino==3) {
+        nuevo_final=2
+    }
+    if (origen==2&&destino==3) {
+        nuevo_final=1
+    }
+    if (origen==2&&destino==1) {
+        nuevo_final=3
+    }
+    if (origen==3&&destino==2) {
+        nuevo_final=1
+    }
+    if (origen==3&&destino==1) {
+        nuevo_final=2
+    }
+    hanoi(n-1,origen,nuevo_final)
+        respuesta= respuesta + origen + " -> " + destino + "&nbsp;"+ "&nbsp;"
+        nuevo_origen = nuevo_final
+        nuevo_final=destino
+        hanoi(n-1, nuevo_origen, nuevo_final)
+    }
+}
+if(destino==origen)
 {
     console.log("Ya está resuelto")
     respuesta="Ya está resuelto"
 }else{
-hanoi(discos,origen,final)
+hanoi(n,origen,destino)
 console.log(respuesta)
 respuesta= "El resultado es " + respuesta
 }
-let contenedor = document.querySelector("#texto")
+let contenedor = document.querySelector("#contenedor")
 contenedor.innerHTML = respuesta
