51a867c1370604d66e60ae4014878671ec33150f
Jorgecc15 <92415699+Jorgecc15@users.noreply.github.com>
2022-05-09 20:31:40 -0500

    Update hanoi.js

--- hanoi.js
+++ hanoi.js
@@ -1,29 +1,29 @@
 let respuesta=""
 
-function hanoi(discos,origen,destino) {
+function hanoi(discos,origen,final) {
     if (discos==1) {
-        respuesta=respuesta + origen + " -> " + destino + "&nbsp;"+ "&nbsp;"
+        respuesta=respuesta + origen + " -> " + final + "&nbsp;"+ "&nbsp;"
     } else{
-        if (destino==3&&origen==1) {
+        if (final==3&&origen==1) {
             nuevo_final=2
         }
-        if (destino==3&&origen==2) {
+        if (final==3&&origen==2) {
             nuevo_final=1
         }
-        if (destino==2&&origen==1) {
+        if (final==2&&origen==1) {
             nuevo_final=3
         }
-        if (destino==2&&origen==3) {
+        if (final==2&&origen==3) {
             nuevo_final=1
         }
-        if (destino==1&&origen==3) {
+        if (final==1&&origen==3) {
             nuevo_final=2
         }
-        if (destino==1&&origen==2) {
+        if (final==1&&origen==2) {
             nuevo_final=3
         }
         hanoi(discos-1,origen,nuevo_final)
-        respuesta= respuesta + origen + " -> " + destino + "&nbsp;"+ "&nbsp;"
+        respuesta= respuesta + origen + " -> " + final + "&nbsp;"+ "&nbsp;"
         nuevo_origen = nuevo_final
         nuevo_final=final
         hanoi(discos-1, nuevo_origen, nuevo_final)
@@ -37,15 +37,15 @@ do {
     origen=Number(prompt("¿Cual es el orgien de los discos? (1, 2 o 3)"))    
 } while (origen!=1 && origen!=2 && origen!=3 || isNaN(origen));
 do {
-    destino=Number(prompt("¿Cual es el destino de los discos? (1, 2 o 3)"))    
-} while (destino!=1 && destino!=2 && destino!=3 || isNaN(destino));
+    final=Number(prompt("¿Cual es el destino de los discos? (1, 2 o 3)"))    
+} while (final!=1 && final!=2 && final!=3 || isNaN(final));
 
-if(destino==origen)
+if(final==origen)
 {
     console.log("Ya está resuelto")
     respuesta="Ya está resuelto"
 }else{
-hanoi(discos,origen,destino)
+hanoi(discos,origen,final)
 console.log(respuesta)
 respuesta= "El resultado es " + respuesta
 }
